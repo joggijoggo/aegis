00144b3d58f98829b854222bec98eb832068f71d
Nicolas Fabretti <n.fabretti@lexfo.fr>
2026-07-28 12:03:43 +0200

    remove python comment in run_lint.sh

--- run_lint.sh
+++ run_lint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-"""Aegis Framework - Lint Execution Runner.
-
-Activates the local virtual environment and executes code compliance checks.
-"""
 
 # Active local virtual environment isolation layers
 source .venv/bin/activate
